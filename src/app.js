c33c0487eea673b18454226e4cfb5399e8d9119d
RikrdoLeal <140850677+RikrdoLeal@users.noreply.github.com>
2024-04-04 16:28:28 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document
+    .querySelector("#newButton")
+    .addEventListener("click", randomCardGenerator);
 };
+
+function randomCardGenerator() {
+  const simbols = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let colors = [];
+
+  function randomGenerate(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return randomIndex;
+  }
+
+  const randomSimbol = simbols[randomGenerate(simbols)];
+  const randomNumber = numbers[randomGenerate(numbers)];
+
+  const getNumber = document.getElementById("card-number");
+  getNumber.innerHTML = randomNumber;
+
+  const getRightSimbol = document.getElementById("right-simbol");
+  const getLeftSimbol = document.getElementById("left-simbol");
+  getRightSimbol.innerHTML = randomSimbol;
+  getLeftSimbol.innerHTML = randomSimbol;
+
+  colors =
+    randomSimbol === "&hearts;" || randomSimbol === "&diams;" ? "red" : "";
+  getRightSimbol.style.color =
+    randomSimbol === "&hearts;" || randomSimbol === "&diams;" ? "red" : "";
+  getLeftSimbol.style.color =
+    randomSimbol === "&hearts;" || randomSimbol === "&diams;" ? "red" : "";
+}
